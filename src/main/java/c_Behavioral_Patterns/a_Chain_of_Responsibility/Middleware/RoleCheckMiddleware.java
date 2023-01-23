74e004bb5019572f5a5a2fb5c10de91481d0e006
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-01-23 14:14:47 +0000

    Chain of Responsibility Design Pattern

---
+++ src/main/java/c_Behavioral_Patterns/a_Chain_of_Responsibility/Middleware/RoleCheckMiddleware.java
@@ -0,0 +1,14 @@
+package c_Behavioral_Patterns.a_Chain_of_Responsibility.Middleware;
+
+// ConcreteHandler. Checks a user's role.
+
+public class RoleCheckMiddleware extends Middleware {
+    public boolean check(String email, String password) {
+        if (email.equals("admin@example.com")) {
+            System.out.println("Hello, admin!");
+            return true;
+        }
+        System.out.println("Hello, user!");
+        return checkNext(email, password);
+    }
+}
